da0a6acbe27289079e6540a6438ae6b273f52f04
yanmiller-rus <75687988+yanmiller-rus@users.noreply.github.com>
2020-12-16 23:25:21 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+#total planetary weight of the solar system
+exec('''planet_Mercury = 3.285E23
+planet_Venera = 4.867E24
+planet_Earth = 5.972E24
+planet_Mars = 6.39E23
+planet_Jupiter = 1.898E27
+planet_Saturn = 5.683E26
+planet_Uran = 8.681E25
+planet_Neptun = 1.024E26
+
+weight = planet_Mercury + planet_Venera + planet_Earth + planet_Mars + planet_Jupiter + planet_Saturn + planet_Uran + planet_Neptun
+
+print("Общий планетарный вес Солнечной системы: %s " % weight)
+''')
+
